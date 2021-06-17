350d185d4c885f8539ba8bbe6c96a3d611c1ba10
Milan Maharana <milan6214@gmail.com>
2021-06-17 23:59:36 +0530

    Designed framework structure

---
+++ src/main/java/com/qa/pages/actions/HomePage.java
@@ -0,0 +1,17 @@
+package com.qa.pages.actions;
+
+import org.openqa.selenium.WebDriver;
+
+import com.qa.pages.locators.HomePageLocators;
+
+public class HomePage {
+
+	public WebDriver driver;
+	public HomePageLocators homepage;
+	
+	
+	public HomePage() {
+		
+		this.
+	}
+}
---
+++ src/test/java/com/qa/testcases/HomePageTest.java
@@ -0,0 +1,14 @@
+package com.qa.testcases;
+
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+
+	
+	@Test
+	public void m1()
+	{
+		
+		System.out.println("hii");
+	}
+}
