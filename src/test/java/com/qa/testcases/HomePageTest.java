557904dea2dba09177eb12b15231a444f51c70f8
Milan Maharana <milan6214@gmail.com>
2021-06-21 01:38:20 +0530

    Added listeners file

--- src/test/java/com/qa/testcases/HomePageTest.java
+++ src/test/java/com/qa/testcases/HomePageTest.java
@@ -1,9 +1,37 @@
 package com.qa.testcases;
 
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import com.qa.base.Page;
+import com.qa.pages.actions.HomePage;
 
 public class HomePageTest {
 
+	HomePage home= new HomePage();
 	
+	@BeforeMethod
+	public void start()
+	{
+		
+		Page.initConfiguration();
+	}
 	
+	@Test()
+	public void verifyHomelogo()
+	{
+		
+		System.out.println(home.verifyHomePageLogo());
+	}
+
+
+	@Test
+	public void verifyLogin()
+	{
+		
+		Assert.assertEquals(home.verifyLogin(), "hii", "failed");
+	}
 }
+
